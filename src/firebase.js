49a13da6ffb5464f53cc482cefaa704b7a2c741f
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-09-22 17:52:16 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCQ5hUv3jMjbIcyeC_DMMavk-WWcVHaQ4",
+  authDomain: "disney-clone-445cb.firebaseapp.com",
+  projectId: "disney-clone-445cb",
+  storageBucket: "disney-clone-445cb.appspot.com",
+  messagingSenderId: "427401649719",
+  appId: "1:427401649719:web:160cfc090a31ed24f2e9b2",
+  measurementId: "G-TRT0ZN3D00"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
